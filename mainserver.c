858ab115e2f7a32cc68d8232ca5d8c546eb31519
asthatyagi <32592786+asthatyagi@users.noreply.github.com>
2018-08-25 13:17:03 +0530

    Initial Commit

---
+++ mainserver.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<pthread.h>
+
+#define PORT 4444
+#define BUF_SIZE 65535
+#define Client_Address_Length 100
+
+void * receiveMessage(void * socket) 
+{
+	 int sockfd, ret;
+	 char buffer[BUF_SIZE]; 
+	 sockfd = (int) socket;
+	 memset(buffer, 0, BUF_SIZE);  
+	while(1)
+	{
+		  ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);  
+		  if (ret < 0)   
+		   printf("Error receiving data!\n");    
+		   else 
+		  {
+		   printf("client: ");
+		   fputs(buffer, stdout);
+		   //printf("\n");
+		  }  
+	 }
+}
+
+void main() {
+ struct sockaddr_in addr, cl_addr;
+ int sockfd, len, ret, newsockfd;
+ char buffer[BUF_SIZE];
+ pid_t childpid;
+ char clientAddr[Client_Address_Length];
+ pthread_t rThread;
+ 
+ sockfd = socket(AF_INET, SOCK_STREAM, 0);
+ if (sockfd < 0) {
+  printf("Error creating socket!\n");
+  exit(1);
+ }
+ printf("Socket created...\n");
+ 
+ memset(&addr, 0, sizeof(addr));
+ addr.sin_family = AF_INET;
+ addr.sin_addr.s_addr = INADDR_ANY;
+ addr.sin_port = PORT;
+ 
+ ret = bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));
+ if (ret < 0) {
+  printf("Error binding!\n");
+  exit(1);
+ }
+ printf("Binding done...\n");
+
+ printf("Waiting for a connection...\n");
+ listen(sockfd, 5);
+
+
+ len = sizeof(cl_addr);
+ newsockfd = accept(sockfd, (struct sockaddr *) &cl_addr, &len);
+ if (newsockfd < 0) {
+  printf("Error in accepting connection!\n");
+  exit(1);
+ } 
+
+ inet_ntop(AF_INET, &(cl_addr.sin_addr), clientAddr, Client_Address_Length);
+ printf("Connection accepted from %s...\n", clientAddr); 
+ 
+ memset(buffer, 0, BUF_SIZE);
+ printf("Enter your message\n");
+
+ //creating a new thread for receiving messages from the client
+ ret = pthread_create(&rThread, NULL, receiveMessage, (void *) newsockfd);
+ if (ret) {
+  printf("error in pthread %d\n", ret);
+  exit(1);
+ }
+
+ while (fgets(buffer, BUF_SIZE, stdin) != NULL) {
+  ret = sendto(newsockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &cl_addr, len);  
+  if (ret < 0) {  
+   printf("Error sending data!\n");  
+   exit(1);
+  }
+ }   
+ 
+ close(newsockfd);
+ close(sockfd);
+
+ pthread_exit(NULL);
+ return;
+}
